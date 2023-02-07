08c87f2fd8ad6c7e6f6404e81cf13a6a8224a0f6
etogood <ilya@shaura.ru>
2023-02-07 19:16:02 +0300

    Добавьте файлы проекта.

---
+++ SledgePlus/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SledgePlus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
