e98daacc6f2800b19b17f15436413587c65b4090
etogood <ilya@shaura.ru>
2023-02-27 10:37:53 +0300

    HTML IDE add

--- SledgePlus.WPF/Commands/Navigation/ToIDECommand.cs
+++ SledgePlus.WPF/Commands/Navigation/ToIDECommand.cs
@@ -1,11 +1,25 @@
-﻿namespace SledgePlus.WPF.Commands.Navigation;
+﻿using Microsoft.Extensions.DependencyInjection;
+using SledgePlus.WPF.Factories;
+using SledgePlus.WPF.Stores.Navigation;
+using SledgePlus.WPF.ViewModels.UserControls;
+
+namespace SledgePlus.WPF.Commands.Navigation;
 
 public class ToIDECommand : Command
 {
+    private readonly INavigationStore _navigationStore;
+    private readonly IFactory<ViewModel> _viewModelFactory;
+
+    public ToIDECommand(IHost host)
+    {
+        _navigationStore = host.Services.GetRequiredService<INavigationStore>();
+        _viewModelFactory = host.Services.GetRequiredService<IFactory<ViewModel>>();
+    }
+
     public override bool CanExecute(object? parameter) => true;
 
     public override void Execute(object? parameter)
     {
-        throw new NotImplementedException();
+        _navigationStore.CurrentViewModel = _viewModelFactory.Get(typeof(IDEViewModel));
     }
 }
--- SledgePlus.WPF/Factories/ViewModelFactory.cs
+++ SledgePlus.WPF/Factories/ViewModelFactory.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 using SledgePlus.WPF.ViewModels.UserControls;
 
 namespace SledgePlus.WPF.Factories;
@@ -18,6 +16,10 @@ public class ViewModelFactory : IFactory<ViewModel>
     {
         if (t == typeof(AuthenticationViewModel)) return _host.Services.GetRequiredService<AuthenticationViewModel>();
         if (t == typeof(SignInViewModel)) return _host.Services.GetRequiredService<SignInViewModel>();
+        if (t == typeof(IDEViewModel)) return _host.Services.GetRequiredService<IDEViewModel>();
+        if (t == typeof(LearningMenuViewModel)) return _host.Services.GetRequiredService<LearningMenuViewModel>();
+        if (t == typeof(PersonalAccountViewModel)) return _host.Services.GetRequiredService<PersonalAccountViewModel>();
+        if (t == typeof(UserMenuViewModel)) return _host.Services.GetRequiredService<UserMenuViewModel>();
 
         throw new NotImplementedException("ViewModel Type");
     }
