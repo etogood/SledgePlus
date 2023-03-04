936448cc836d2f7090fa36b66150c9583bd0ee1f
etogood <ilya@shaura.ru>
2023-03-04 14:52:30 +0300

    updated mingw, added implementation of the compile button, but it couses wingw libs errors

--- SledgePlus.WPF/Commands/InnerActions/CompileCodeCommand.cs
+++ SledgePlus.WPF/Commands/InnerActions/CompileCodeCommand.cs
@@ -1,11 +1,32 @@
-﻿namespace SledgePlus.WPF.Commands.InnerActions;
+﻿using System.IO;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using SledgePlus.WPF.Factories;
+using SledgePlus.WPF.ViewModels.UserControls;
+
+namespace SledgePlus.WPF.Commands.InnerActions;
 
 public class CompileCodeCommand : Command
 {
+    private readonly IFactory<ViewModel> _viewModelFactory;
+
+    public CompileCodeCommand(IHost host)
+    {
+        _viewModelFactory = host.Services.GetRequiredService<IFactory<ViewModel>>();
+    }
+
     public override bool CanExecute(object? parameter) => true;
 
     public override void Execute(object? parameter)
     {
-        
+        var text = ((IDEViewModel)_viewModelFactory.Get(typeof(IDEViewModel))).CodeDocument.Text;
+
+        using (var fs = File.Create(Directory.GetCurrentDirectory() + @"\__temp_code.cpp"))
+        {
+            var info = new UTF8Encoding(true).GetBytes(text);
+            fs.Write(info, 0, info.Length);
+        }
+
+        System.Diagnostics.Process.Start("cmd.exe", "/C" + @"MinGW\bin\g++.exe __temp_code.cpp");
     }
 }
